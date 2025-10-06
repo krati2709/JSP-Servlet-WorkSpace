150752b5356dc9188e77c2ac1ff9241764f34483
krati2709 <kratitrivedi27@gmail.com>
2025-10-06 16:18:18 +0530

    new

--- Web-Project/src/com/rays/controller/LoginCtl.java
+++ Web-Project/src/com/rays/controller/LoginCtl.java
@@ -16,48 +16,54 @@ import com.rays.model.UserModel;
 @WebServlet("/LoginCtl")
 public class LoginCtl extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String op = request.getParameter("operation");
-		System.out.println("op -> " + op);
-		// to start login session
+		System.out.println("op ---> " + op);
+
 		if (op != null) {
 			HttpSession session = request.getSession();
-			session.invalidate(); // to end the session
+			session.invalidate(); // Destroy the session
 			request.setAttribute("successMsg", "user logout successfully");
 		}
-		
+
 		RequestDispatcher rd = request.getRequestDispatcher("LoginView.jsp");
 		rd.forward(request, response);
-		
+
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		UserBean bean = new UserBean();
 		UserModel model = new UserModel();
-		
+
 		String login = request.getParameter("login");
 		String password = request.getParameter("password");
-		
+
 		HttpSession session = request.getSession();
-		
+
 		try {
 			bean = model.authenticate(login, password);
-			
+
 			if (bean != null) {
 				System.out.println("user login successfully");
 				session.setAttribute("user", bean);
 				response.sendRedirect("WelcomeCtl");
 			} else {
-				request.setAttribute("errorMsg", "invalid login id or password");
+				request.setAttribute("errorMsg", "invalid login or password");
+				RequestDispatcher rd = request.getRequestDispatcher("LoginView.jsp");
+				rd.forward(request, response);
 			}
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 }
